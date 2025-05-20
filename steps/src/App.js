f435cc69dfa2cf17aa48c512a12f3c201e23f4fe
kirti-krishnn <kirti.krishnn@gmail.com>
2025-05-21 00:13:37 +0530

    first commit

--- steps/src/App.js
+++ steps/src/App.js
@@ -8,7 +8,7 @@ const messages = [
 
 function App() {
   return (
-    <div className="Steps">
+    <div>
       <Steps />
       <Steps />
     </div>
@@ -32,41 +32,53 @@ function Steps() {
   }
 
   return (
-    <div>
-      <button className="close" onClick={() => setIsOpen((isopen) => !isopen)}>
-        &times;
-      </button>
+    <div
+      style={{
+        position: "relative",
+        marginBottom: "2rem",
+        border: "1px solid #ccc",
+        padding: "1rem",
+      }}
+    >
+      <div>
+        <button
+          className="close"
+          onClick={() => setIsOpen((isopen) => !isopen)}
+        >
+          &times;
+        </button>
 
-      {isopen && (
-        <div className="steps">
-          <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
+        {isopen && (
+          <div className="steps">
+            <div className="numbers">
+              <div className={step >= 1 ? "active" : ""}>1</div>
+              <div className={step >= 2 ? "active" : ""}>2</div>
+              <div className={step >= 3 ? "active" : ""}>3</div>
+            </div>
+
+            <p className="message">
+              {step === 0 ? `make a selection` : `${messages[step - 1]}`}
+            </p>
+
+            <div className="buttons">
+              <button
+                className="previous"
+                onClick={handlerPrevious}
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              >
+                Previous
+              </button>
+              <button
+                className="next"
+                onClick={handlerNext}
+                style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              >
+                Next
+              </button>
+            </div>
           </div>
-
-          <p className="message">
-            {step === 0 ? `make a selection` : `${messages[step - 1]}`}
-          </p>
-
-          <div className="buttons">
-            <button
-              className="previous"
-              onClick={handlerPrevious}
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            >
-              Previous
-            </button>
-            <button
-              className="next"
-              onClick={handlerNext}
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-            >
-              Next
-            </button>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
