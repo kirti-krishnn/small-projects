021c7ed9562279c17938ba2ae37082689442000d
kirti-krishnn <kirti.krishnn@gmail.com>
2025-05-20 16:46:23 +0530

    first commit

---
+++ steps/src/App.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+function App() {
+  return (
+    <div className="Steps">
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+
+function Steps() {
+  const [step, setStep] = useState(1);
+
+  function handlerNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
+  }
+
+  function handlerPrevious() {
+    if (step > 1) {
+      setStep((s) => s - 1);
+    }
+  }
+
+  return (
+    <div>
+      <button className="close">&times;</button>
+      <div className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
+
+        <p className="message">{`${messages[step - 1]}`}</p>
+
+        <div className="buttons">
+          <button
+            className="previous"
+            onClick={handlerPrevious}
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          >
+            Previous
+          </button>
+          <button
+            className="next"
+            onClick={handlerNext}
+            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
