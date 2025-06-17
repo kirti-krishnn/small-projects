57fe3b38cd4a163b909612d0ba10f7f7498632b3
kirti-krishnn <kirti.krishnn@gmail.com>
2025-06-17 22:01:55 +0530

    first commit

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -92,51 +92,63 @@ function Main() {
       </div>
       <div className="box">
         <button className="btn-toggle">-</button>
-        <div className="summary">
-          <h2>Movies you watched</h2>
-          <div>
-            <p>
-              <span>#️⃣</span>
-              <span>{watched.length} movies</span>
-            </p>
-            <p>
-              <span>⭐️</span>
-              <span>{avgImdbRating}</span>
-            </p>
-            <p>
-              <span>🌟</span>
-              <span>{avgUserRating}</span>
-            </p>
-            <p>
-              <span>⏳</span>
-              <span>{avgRuntime} min</span>
-            </p>
-          </div>
-        </div>
+        <Summary />
         <ul className="list">
           {watched.map((movie) => (
-            <li key={movie.imdbID}>
-              <img src={movie.Poster} alt={`${movie.Title} poster`} />
-              <h3>{movie.Title}</h3>
-              <div>
-                <p>
-                  <span>⭐️</span>
-                  <span>{movie.imdbRating}</span>
-                </p>
-                <p>
-                  <span>🌟</span>
-                  <span>{movie.userRating}</span>
-                </p>
-                <p>
-                  <span>⏳</span>
-                  <span>{movie.runtime} min</span>
-                </p>
-              </div>
-            </li>
+            <Watched movie={movie} key={movie.imdbID} />
           ))}
         </ul>
       </div>
     </div>
+  );
+}
+
+function Summary() {
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>{avgImdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{avgRuntime} min</span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function Watched({ movie }) {
+  return (
+    <li key={movie.imdbID}>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>⭐️</span>
+          <span>{movie.imdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{movie.userRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{movie.runtime} min</span>
+        </p>
+      </div>
+    </li>
   );
 }
 
